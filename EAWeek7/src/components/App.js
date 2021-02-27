45ac3c73985e85f74b80ba0c1f8c9220d0bded89
Erin Andrews <77820664+EMarie28@users.noreply.github.com>
2021-02-26 21:10:06 -0500

    Add files via upload

---
+++ EAWeek7/src/components/App.js
@@ -0,0 +1,14 @@
+import Sets from '.components/Sets'
+import Squats from '.components/Squats'
+
+function App() {
+    return(
+        <div>
+        <h1> Squat Tracker </h1>
+        <Sets/>
+        <Squats/>
+        </div>
+    );
+}
+
+export default App;
